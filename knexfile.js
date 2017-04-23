3dad728f60ae28405d545f40d87a30d86b43bfb2
Tomasz <tomaszkogowski@gmail.com>
2017-04-23 17:25:12 +0200

    Added database schema(migration and basic queries) and password encryption.

---
+++ knexfile.js
@@ -0,0 +1,16 @@
+//knexfile.js
+
+module.exports = {
+
+    production: {
+        client: 'sqlite3',
+        useNullAsDefault: true,
+        connection: {
+            filename: './db/chat_users.db'
+        },
+        migrations: {
+            directory: './db/migrations'
+        }
+    }
+
+};
